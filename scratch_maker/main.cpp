2419aaac368de0054f2c9e6b816a4a553f32522f
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-09-14 00:37:16 -0300

    Chanding main to use Shader class

--- scratch_maker/main.cpp
+++ scratch_maker/main.cpp
@@ -3,6 +3,8 @@
 #include <glad/glad.h>
 #include <glfw3.h>
 
+#include "Shader/Shader.h"
+
 const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) 
@@ -38,6 +40,46 @@ int main()
 		std::cout << "Failed to initialize GLAD" << std::endl;
 	}
 
+	Shader shader("C:/croc/scratch_maker/scratch_maker/assets/shaders/vertex.glsl",
+		"C:/croc/scratch_maker/scratch_maker/assets/shaders/fragment.glsl");
+
+	float vertices[] = {
+		//posistions          //colors
+         0.5f,  0.5f, 0.0f,   0.3f, 0.3f, 0.3f,  // upper right
+         0.5f, -0.5f, 0.0f,   0.3f, 0.3f, 0.3f,  // bottom right
+        -0.5f, -0.5f, 0.0f,   0.3f, 0.3f, 0.3f,  // bottom left
+        -0.5f,  0.5f, 0.0f,   0.3f, 0.3f, 0.3f   // upper left
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3, 
+		1, 2, 3
+	};
+	
+	unsigned int VBO, VAO, IBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &IBO);
+
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// colour attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 	while (!glfwWindowShouldClose(window)) 
 	{
 		// input
@@ -45,8 +87,12 @@ int main()
 
 
 		// rendering
-		glClearColor(0.3f, 1.0f, 1.0f, 1.0f);
+		glClearColor(0.3f, 0.3f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.bind();
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// swap buffers and events
 		glfwSwapBuffers(window);
