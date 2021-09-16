9029a4239f5063805c58d3bc7d80ea44daab93e6
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-09-16 00:44:36 -0300

    vertex shader changed to accept transform

--- scratch_maker/assets/shaders/vertex.glsl
+++ scratch_maker/assets/shaders/vertex.glsl
@@ -2,11 +2,14 @@
 
 layout(location = 0) in vec3 pos;
 layout(location = 1) in vec3 color;
+layout(location = 2) in vec3 bottomPos;
 
 out vec3 vertexColor;
 
+uniform mat4 transform;
+
 void main()
-{
-	gl_Position = vec4(pos, 1.0f);
+{	
+	gl_Position = transform * vec4(pos, 1.0f);
 	vertexColor = color;
 }
